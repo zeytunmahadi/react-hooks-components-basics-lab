4338acaeed524bd73c84b03da5cf83a1fb9eb8a4
zeitun <zeytunmahadi718@gmail.com>
2024-06-23 11:20:42 +0300

    have learnt the components of react

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 
 function NavBar() {
   return (
@@ -16,16 +16,24 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return(
+  <div id="about">
+    <h2>About</h2>
 
+  </div>
+  );
+
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
-    </div>
+      <Home  />
+     <About/>
+   
+     </div>
   );
 }
 
